52ef19e87067512d55eaaf7ba7c2aa75bf88cae4
Džiugas Pečiulevičius <dziugaspeciulevicius@gmail.com>
2019-09-11 13:43:08 +0300

    VARIABLES

---
+++ ErrorCalculationExercise/src/com/company/Variables.java
@@ -0,0 +1,15 @@
+package com.company;
+
+public class Variables {
+    public double a;
+    public double b;
+    public double x;
+    public double y;
+
+    public Variables(double a, double b, double x, double y){
+        this.a = a;
+        this.b = b;
+        this.x = x;
+        this.y = y;
+    }
+}
