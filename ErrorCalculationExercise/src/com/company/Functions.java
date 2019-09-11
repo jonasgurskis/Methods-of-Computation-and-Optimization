f8766e7398024dfa6cd88c6363aa7e938d19fd9d
Džiugas Pečiulevičius <dziugaspeciulevicius@gmail.com>
2019-09-11 21:35:27 +0300

    added more methods

--- ErrorCalculationExercise/src/com/company/Functions.java
+++ ErrorCalculationExercise/src/com/company/Functions.java
@@ -9,4 +9,14 @@ public class Functions {
     public static double getRelativeError(double a, double x) {
         return (Math.abs(a - x) / Math.abs(a));
     }
+
+    public static double getAmountError(double a, double b, double x, double y){
+        return Math.abs((a + b)-(x + y));
+    }
+
+    public static double getDifferenceError(double a, double b, double x, double y){
+        return Math.abs((a - b)-(x - y));
+    }
+
+
 }
