146d2d741c2be6853aae986769b86c000f4a3485
Džiugas Pečiulevičius <dziugaspeciulevicius@gmail.com>
2019-09-11 21:35:41 +0300

    Update Main.java

--- ErrorCalculationExercise/src/com/company/Main.java
+++ ErrorCalculationExercise/src/com/company/Main.java
@@ -8,21 +8,24 @@ public class Main {
 
         Scanner input = new Scanner(System.in);
         System.out.println("Enter a number of an \"a\" (exact) variable: ");
-        double input_a = input.nextLong();
+        double input_a = input.nextDouble();
         System.out.println("Enter a number of an \"b\" (exact) variable: ");
-        double input_b = input.nextLong();
+        double input_b = input.nextDouble();
         System.out.println("Enter a number of an \"x\" (approximate) variable: ");
-        double input_x = input.nextLong();
+        double input_x = input.nextDouble();
         System.out.println("Enter a number of an \"y\" (approximate) variable: ");
-        double input_y = input.nextLong();
+        double input_y = input.nextDouble();
 
+        Variables value = new Variables(input_a, input_b, input_x, input_y);
 
-        Variables SumAndDiff = new Variables(input_a, input_b, input_x, input_y);
-
-        System.out.println("The absolute error is: " + Functions.getAbsoluteError(SumAndDiff.a, SumAndDiff.x));
-        System.out.println("The relative error is: " + Functions.getRelativeError(SumAndDiff.a, SumAndDiff.x));
-
-
+        System.out.println("The ABSOLUTE error of \"a\" is: " + Functions.getAbsoluteError(value.a, value.x));
+        System.out.println("The ABSOLUTE error of \"b\" is: " + Functions.getAbsoluteError(value.b, value.y));
+        System.out.println("The RELATIVE error of \"a\" is: " + Functions.getRelativeError(value.a, value.x));
+        System.out.println("The RELATIVE error of \"b\" is: " + Functions.getRelativeError(value.b, value.y));
+        System.out.println("The AMOUNT error is: " +
+                Functions.getAmountError(value.a, value.b, value.x, value.y));
+        System.out.println("The DIFFERENCE error is: " +
+                Functions.getDifferenceError(value.a, value.b, value.x, value.y));
 
     }
 }
