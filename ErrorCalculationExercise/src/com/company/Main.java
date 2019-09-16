704d7dd7ae8d9782b3cedbfb593b689d3fd234ed
Džiugas Pečiulevičius <dziugaspeciulevicius@gmail.com>
2019-09-16 21:56:25 +0300

    Update Main.java

--- ErrorCalculationExercise/src/com/company/Main.java
+++ ErrorCalculationExercise/src/com/company/Main.java
@@ -15,15 +15,15 @@ public class Main {
         System.out.println("Enter a number of an \"y\" (approximate) variable: ");
         double input_y = input.nextDouble();
 
-        Variables value = new Variables(input_a, input_b, input_x, input_y);
+        Class value = new Class(input_a, input_b, input_x, input_y);
 
-        System.out.println("The ABSOLUTE error of \"a\" is: " + Functions.getAbsoluteError(value.a, value.x));
-        System.out.println("The ABSOLUTE error of \"b\" is: " + Functions.getAbsoluteError(value.b, value.y));
-        System.out.println("The RELATIVE error of \"a\" is: " + Functions.getRelativeError(value.a, value.x));
-        System.out.println("The RELATIVE error of \"b\" is: " + Functions.getRelativeError(value.b, value.y));
+        System.out.println("The ABSOLUTE error of \"a\" is: " + Class.getAbsoluteError(value.a, value.x));
+        System.out.println("The ABSOLUTE error of \"b\" is: " + Class.getAbsoluteError(value.b, value.y));
+        System.out.println("The RELATIVE error of \"a\" is: " + Class.getRelativeError(value.a, value.x));
+        System.out.println("The RELATIVE error of \"b\" is: " + Class.getRelativeError(value.b, value.y));
         System.out.println("The AMOUNT error is: " +
-                Functions.getAmountError(value.a, value.b, value.x, value.y));
+                Class.getAmountError(value.a, value.b, value.x, value.y));
         System.out.println("The DIFFERENCE error is: " +
-                Functions.getDifferenceError(value.a, value.b, value.x, value.y));
+                Class.getDifferenceError(value.a, value.b, value.x, value.y));
     }
 }
